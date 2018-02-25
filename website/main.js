e0abfd71171405cb69c21ff1e5c7bac05184d3c0
dromid21 <dromid21@gmail.com>
2018-02-24 20:04:30 -0800

    Javascript developing

---
+++ website/main.js
@@ -0,0 +1,8 @@
+// const answer = prompt(question);
+
+const start = prompt('Enter the start:');
+const step = prompt('Enter the step:');
+const end = prompt('Enter the end:')
+
+
+// alert(`You answered ${answer}`);
