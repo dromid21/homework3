6901695ebd85527251c42c0d99c8ada1127c821f
dromid21 <dromid21@gmail.com>
2018-02-25 12:15:35 -0800

    blah blah

--- website/main.js
+++ website/main.js
@@ -1,8 +1,14 @@
 // const answer = prompt(question);
 
-const start = prompt('Enter the start:');
-const step = prompt('Enter the step:');
-const end = prompt('Enter the end:')
+const p_start = prompt('Enter the start:');
+const p_step = prompt('Enter the step:');
+const p_end = prompt('Enter the end:');
+
+const start = number(p_start);
+const step = number(p_step);
+const end = number(p_end);
+
+const my_array = []
 
 
 // alert(`You answered ${answer}`);
