59f7f192318cf1939af58e7ff2f9ea87dc76cbcd
dromid21 <dromid21@gmail.com>
2018-02-27 20:45:47 -0800

    All array functions included

--- website/main.js
+++ website/main.js
@@ -12,12 +12,13 @@ const step = parseInt(p_step, 10);
 const end = parseInt(p_end, 10);
 
 
-// Initialize array and sum functions
+// Initialize array, sum, and binary objects that will be filled in through if loop
 
 const my_array = new Array();
-const array_sum = 0;
+var sumarray = 0;
+const bin_array = new Array();
 
-// 'If' statement
+// 'If' statement. 
 
 if (isNaN(start) || isNaN(step) || isNaN(end) || step == 0 ||
 step > 0 && start > end || step < 0 && start < end){
@@ -33,5 +34,15 @@ step > 0 && start > end || step < 0 && start < end){
             my_array.push(start)
         }
     }
-}
+    for (var i = 0; i < my_array.length; i++) {
+        sumarray += my_array[i]
+    }
+    for (var i = 0; i < my_array.length; i++) {
+        bin_array.push(my_array[i].toString(2))
+    }
+
+
 document.write("The generated array is: " + my_array)
+document.write("<br/> The sum is: " + sumarray)
+document.write("</br> The binary of absolute element values are: " + bin_array)
+}
