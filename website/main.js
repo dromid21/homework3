e842895f5933e39e6390eaa45d1b7d0381b0a54e
dromid21 <dromid21@gmail.com>
2018-02-27 19:43:37 -0800

    First array working

--- website/main.js
+++ website/main.js
@@ -4,11 +4,34 @@ const p_start = prompt('Enter the start:');
 const p_step = prompt('Enter the step:');
 const p_end = prompt('Enter the end:');
 
-const start = number(p_start);
-const step = number(p_step);
-const end = number(p_end);
 
-const my_array = []
+// Parse values into integers
+
+let start = parseInt(p_start, 10);
+const step = parseInt(p_step, 10);
+const end = parseInt(p_end, 10);
 
 
-// alert(`You answered ${answer}`);
+// Initialize array and sum functions
+
+const my_array = new Array();
+const array_sum = 0;
+
+// 'If' statement
+
+if (isNaN(start) || isNaN(step) || isNaN(end) || step == 0 ||
+step > 0 && start > end || step < 0 && start < end){
+    document.write('Invalid integer input, please reload and try again.');
+} else {
+    if (step > 0) {
+        for (start; start <= end; start += step) {
+            my_array.push(start);
+        }
+    }
+    else {
+        for (start; start >= end; start += step) {
+            my_array.push(start)
+        }
+    }
+}
+document.write("The generated array is: " + my_array)
